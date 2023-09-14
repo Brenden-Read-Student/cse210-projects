edbeab82f453102efa20153fb295f2639bc3106a
Brenden Read <rea20010@byui.edu>
2023-09-14 17:36:35 -0600

    Edits to prep I

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("What is your first name?");
         string first_name = Console.ReadLine();
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your last name?");
         string last_name = Console.ReadLine();
-        Console.WriteLine("What is your last name?");
         Console.WriteLine(" ");
         Console.Write($"Your name is {first_name}, {last_name} {first_name}");
     }
