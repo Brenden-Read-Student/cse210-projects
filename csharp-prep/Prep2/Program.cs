3ca44c8f7e2566f1c6e34815142c0a5f8776f25b
Brenden Read <rea20010@byui.edu>
2023-09-14 17:51:58 -0600

    Prep II

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.IO.Pipes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Final Grade persentage? ");
+        string persentage = Console.ReadLine();
+        int grade = int.Parse(persentage);
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your final letter grade Is A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your final letter grade Is B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your final letter grade Is C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your final letter grade Is D");
+        }
+        else if (grade < 50)
+        {
+            Console.WriteLine("Your final letter grade Is F");
+        }
+        else
+        {
+            Console.WriteLine("That is not an acceptable grade. Make sure you only provide whole numbers");
+        }
     }
 }
